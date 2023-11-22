267121f532e1879d7bc8e8947732a53df3b80f71
Simon Franklin <117857690+simonfranklin1@users.noreply.github.com>
2023-11-22 20:29:33 -0300

    Finalizando calculadora

--- js/index.ts
+++ js/index.ts
@@ -1,47 +1,25 @@
+// Elements
+
 const containerResult = document.querySelector('.result') as HTMLElement;
 const numberBtns = document.querySelectorAll('.number');
 const operatorsBtn = document.querySelectorAll('.sign');
 const finish = document.querySelector('.finish') as HTMLButtonElement;
+const clearBtn = document.querySelector('.clear') as HTMLButtonElement
+const negativeBtn = document.querySelector('.negative') as HTMLButtonElement;
+const byHundredBtn = document.querySelector('.percent') as HTMLButtonElement;
 
-type num = string | boolean | number;
+type num = string | boolean;
 
 let n1: num = false;
 let n2: num = false;
 
 let operator: string  = '';
 
+// Functions
+
 const showNumber = () :void => {
     containerResult!.innerText = `${n1.toString().replace('.', ',')}`
 }
-
-numberBtns.forEach((btn: any) :void => {
-    btn.addEventListener('click', () :void => {
-        if(!n1) {
-            n1 = btn.value;
-        } else {
-            n1 += btn.value;
-        }
-
-        showNumber()
-    })
-})
-
-operatorsBtn.forEach((btn: any) :void => {
-    btn.addEventListener('click', () :void => {
-        if(!n1) {
-            return
-        } else {
-            document.querySelector('.selected')?.classList.remove('selected');
-            btn.classList.add('selected');
-            operator = btn.value;
-            
-            showNumber();
-            n2 = n1;
-            n1 = false;
-            console.log(n1, n2)
-        }
-    })
-})
 
 const operation = (number2: number, number1: number, sign: string) => {
     let result: number = 0;
@@ -63,5 +41,75 @@ const operation = (number2: number, number1: number, sign: string) => {
     n1 = `${result}`;
     showNumber();
 }
+
+const resetNumbers = () :void => {
+    n1 = false;
+    n2 = false;
+    operator = '';
+
+    containerResult.innerText = '0';
+    clearBtn.innerText = "AC"
+}
+
+const negativeNumber = () :void => {
+    if(n1 === false) {
+        return;
+    } else {
+        let number: number;
+        number = +n1;
+        number *= -1;
+        n1 = `${number}`;
+        showNumber();
+    }  
+}
+
+const divideByHundred = () :void => {
+    if(!n1) {
+        return;
+    } else {
+        let number: number;
+        number = +n1;
+        number /= 100;
+        n1 = `${number}`;
+        showNumber();
+    }
+}
+
+// Events
+
+numberBtns.forEach((btn: any) :void => {
+    btn.addEventListener('click', () :void => {
+        if(!n1) {
+            n1 = btn.value;
+        } else {
+            n1 += btn.value;
+        }
+
+        showNumber();
+        clearBtn.innerText = "C";
+    })
+})
+
+operatorsBtn.forEach((btn: any) :void => {
+    btn.addEventListener('click', () :void => {
+        if(!n1) {
+            return
+        } else {
+            document.querySelector('.selected')?.classList.remove('selected');
+            btn.classList.add('selected');
+            operator = btn.value;
+            
+            showNumber();
+            n2 = n1;
+            n1 = false;
+        }
+    })
+})
+
+negativeBtn.addEventListener('click', negativeNumber);
+
+clearBtn.addEventListener('click', resetNumbers);
+
+byHundredBtn.addEventListener('click', divideByHundred)
 
 finish.addEventListener('click', () => operation(+n2, +n1, operator));
