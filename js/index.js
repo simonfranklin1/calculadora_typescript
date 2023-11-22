792d0bec2b6869a81607874cfd84bc18fe693513
Simon Franklin <117857690+simonfranklin1@users.noreply.github.com>
2023-11-22 19:43:05 -0300

    logica numeros e operadores

--- js/index.js
+++ js/index.js
@@ -1,1 +1,35 @@
-console.log("Funcionou");
+"use strict";
+const containerResult = document.querySelector('.result');
+const numberBtns = document.querySelectorAll('.number');
+const operatorsBtn = document.querySelectorAll('.sign');
+let n1 = false;
+let n2 = false;
+let operator = '';
+numberBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        if (!n1) {
+            n1 = btn.value;
+        }
+        else {
+            n1 += btn.value;
+        }
+        containerResult.innerText = `${n1}`;
+    });
+});
+operatorsBtn.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        var _a;
+        if (!n1) {
+            return;
+        }
+        else {
+            (_a = document.querySelector('.selected')) === null || _a === void 0 ? void 0 : _a.classList.remove('selected');
+            btn.classList.add('selected');
+            operator = btn.value;
+            containerResult.innerText = `${n1}`;
+            n2 = n1;
+            n1 = false;
+            console.log(n1, n2);
+        }
+    });
+});
--- js/index.ts
+++ js/index.ts
@@ -1,1 +1,39 @@
-console.log("Funcionou")
+const containerResult = document.querySelector('.result') as HTMLElement;
+const numberBtns = document.querySelectorAll('.number');
+const operatorsBtn = document.querySelectorAll('.sign');
+
+type num = string | boolean;
+
+let n1: num = false;
+let n2: num = false;
+
+let operator: string  = '';
+
+numberBtns.forEach((btn: any) :void => {
+    btn.addEventListener('click', () :void => {
+        if(!n1) {
+            n1 = btn.value;
+        } else {
+            n1 += btn.value;
+        }
+
+        containerResult.innerText = `${n1}`;
+    })
+})
+
+operatorsBtn.forEach((btn: any) :void => {
+    btn.addEventListener('click', () :void => {
+        if(!n1) {
+            return
+        } else {
+            document.querySelector('.selected')?.classList.remove('selected');
+            btn.classList.add('selected');
+            operator = btn.value;
+            
+            containerResult!.innerText = `${n1}`;
+            n2 = n1;
+            n1 = false;
+            console.log(n1, n2)
+        }
+    })
+})
